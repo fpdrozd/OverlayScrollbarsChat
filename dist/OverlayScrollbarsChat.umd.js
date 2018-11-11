66b7b445fbb8d86a9f21696eb7858fed14eb6df5
fpdrozd <fpdrozd@gmail.com>
2018-11-11 15:01:26 +0100

    Updated the babel and rollup to compile a umd bundle

--- dist/OverlayScrollbarsChat.umd.js
+++ dist/OverlayScrollbarsChat.umd.js
@@ -5,43 +5,44 @@
   (global.OverlayScrollbarsChat = factory());
 }(this, (function () { 'use strict';
 
-  function scrollBehavior (scrollbar, elements, content, oldContentHeight, appendScroll) {
-    const calculate = () => {
-      const position = scrollbar.scroll().y.position;
-      const hostHeight = elements.host.clientHeight;
-      const contentHeight = content.clientHeight;
-
-      if (!position) scrollbar.scroll({ y: contentHeight - oldContentHeight });
-      else if (position + hostHeight == oldContentHeight) scrollbar.scroll({ y: contentHeight - hostHeight }, appendScroll.duration, appendScroll.easing);
-
+  function scrollBehavior(scrollbar, elements, content, oldContentHeight, appendScroll) {
+    var calculate = function calculate() {
+      var position = scrollbar.scroll().y.position;
+      var hostHeight = elements.host.clientHeight;
+      var contentHeight = content.clientHeight;
+      if (!position) scrollbar.scroll({
+        y: contentHeight - oldContentHeight
+      });else if (position + hostHeight == oldContentHeight) scrollbar.scroll({
+        y: contentHeight - hostHeight
+      }, appendScroll.duration, appendScroll.easing);
       oldContentHeight = contentHeight;
     };
+
     return calculate;
   }
 
-  function OverlayScrollbarsChat (defaultOptions, framework) {
-    const scrollbar = this;
-    const osChat = {};
+  function OverlayScrollbarsChat(defaultOptions, framework) {
+    var scrollbar = this;
+    var osChat = {};
 
-    let scrollBehavior$$1 = () => {};
+    var scrollBehavior$$1 = function scrollBehavior$$1() {};
 
     osChat.added = function (options) {
-      const appendScroll = framework.extend(true, {}, {
+      var appendScroll = framework.extend(true, {}, {
         appendScroll: {
           duration: 200,
           easing: 'linear'
         }
       }, options).appendScroll;
-
-      const elements = scrollbar.getElements();
-      const content = elements.content.querySelector('div');
-
-      let oldContentHeight = content.clientHeight;
-
+      var elements = scrollbar.getElements();
+      var content = elements.content.querySelector('div');
+      var oldContentHeight = content.clientHeight;
       scrollBehavior$$1 = scrollBehavior(scrollbar, elements, content, oldContentHeight, appendScroll);
     };
 
-    osChat.removed = function () { scrollBehavior$$1 = () => {}; };
+    osChat.removed = function () {
+      scrollBehavior$$1 = function scrollBehavior$$1() {};
+    };
 
     osChat.on = function (ev) {
       if (ev == 'contentSizeChanged') scrollBehavior$$1();
--- rollup.umd.js
+++ rollup.umd.js
@@ -1,9 +1,11 @@
 import config from './rollup.es6.js';
+import babel from 'rollup-plugin-babel';
 
 const packageInfo = require('./package.json');
 
 config.output.format = 'umd';
 config.output.name = 'OverlayScrollbarsChat';
 config.output.file = 'dist/OverlayScrollbarsChat.umd.js';
+config.plugins = [ babel() ];
 
 export default config;
